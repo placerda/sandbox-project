f332770a628d3062216a2d0b76e8a360d91dfc4b
Paulo Lacerda <pclacerda@gmail.com>
2024-07-25 10:09:55 -0300

    my feature

--- evaluations/eval_chat_prompt.py
+++
@@ -1,8 +0,0 @@
-from promptflow.core import Prompty
-
-# load prompty as a flow
-f = Prompty.load(source="./src/chat.prompty")
-
-# execute the flow as function
-result = f(question="What is the capital of France?", documents=[])
-print(result)
